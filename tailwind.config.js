29d3e2a5478f57540138039c52eda6439d0393b6
iyiy2008 <yusupovismoiljon1204@gmail.com>
2023-10-26 21:54:03 +0500

    I added the font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 const withMT = require("@material-tailwind/react/utils/withMT");
- 
+
 module.exports = withMT({
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'jost': ['"Jost", "sans-serif"'],
+      },
+    },
   },
   plugins: [],
 });
