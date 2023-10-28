02ccb9a4dbf190c17b87b7475f81a1143ebf3767
iyiy2008 <yusupovismoiljon1204@gmail.com>
2023-10-28 19:39:48 +0500

    add Home

---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Hero from './Hero'
+import Tavar from './Tavar'
+
+const Home = () => {
+    return (
+        <>
+            <Hero />
+            <Tavar />
+        </>
+    )
+}
+
+export default Home
