8d0c804d1a074b7cb5092092fe3824ceecbd15a1
iyiy2008 <yusupovismoiljon1204@gmail.com>
2023-10-28 19:37:57 +0500

    I changed the hero to header

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,14 @@ import {
 } from "react-router-dom";
 import ReactLayouts from './layouts/ReactLayouts';
 import Error404 from './layouts/Error404';
-import Hero from './components/Hero'
+import Home from "./components/Home";
 
 function App() {
 
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route element={<ReactLayouts />}>
-        <Route path='/' element={<Hero />} />
+        <Route path='/' element={<Home />} />
         <Route path='*' element={<Error404 />} />
       </Route>
     )
